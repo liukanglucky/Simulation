868f14b2997b2bc72aaafbf867ed112d4e54408e
wuxiang <644975988@qq.com>
2016-02-29 16:00:58 +0800

    parameter

--- src/main/java/com/platform/dao/ModelDataDao.java
+++ src/main/java/com/platform/dao/ModelDataDao.java
@@ -13,10 +13,10 @@ public interface ModelDataDao {
 	public int insertData2(DATA2 data);
 	public int insertData3A(DATA3A data);
 	public int insertData3B(DATA3B data);
-	public int insertData5A(DATA1 data);
-	public int insertData5B(DATA1 data);
-	public int insertData7(DATA1 data);
-	public int insertData8(DATA1 data);
+	public int insertData5A(DATA5A data);
+	public int insertData5B(DATA5B data);
+	public int insertData7(DATA7 data);
+	public int insertData8(DATA8 data);
 	public Count countUser();
 	public List<DATA1> findDatasByPage(PageBean page);
 	public DATA1 findDatasById(int id);
