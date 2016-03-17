3d43b5fffe4e48572e4d1b9681dae63511195514
wuxiang <644975988@qq.com>
2016-03-17 19:59:24 +0800

    备份实体类

---
+++ src/main/java/com/platform/model/Backup.java
@@ -0,0 +1,33 @@
+package com.platform.model;
+
+public class Backup {
+	private String name;
+	private int id;
+	private String date;
+	private String operater;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getOperater() {
+		return operater;
+	}
+	public void setOperater(String operater) {
+		this.operater = operater;
+	}
+	
+}
