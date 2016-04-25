04e1110afa077e41e14fefed247cb9c02ed3e379
liukanglucky <765759130@qq.com>
2016-04-25 19:34:47 +0800

    maven导出时包含lib下jar，dll默认放在c盘根目录下

--- src/test/java/com/pictem/test/UserTest.java
+++ src/test/java/com/pictem/test/UserTest.java
@@ -12,20 +12,20 @@ public class UserTest {
 	
 	private UserService userService;
     
-    @Before
-    public void before(){                                                                    
-        @SuppressWarnings("resource")
-        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:conf/spring.xml"
-                ,"classpath:conf/spring-mybatis.xml"});
-        userService = (UserService) context.getBean("userServiceImpl");
-    }
-     
-    @Test
-    public void addUser(){
-        User user = new User();
-        user.setName("你好");
-        System.out.println(userService.insertUser(user));
-    }
+//    @Before
+//    public void before(){                                                                    
+//        @SuppressWarnings("resource")
+//        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:conf/spring.xml"
+//                ,"classpath:conf/spring-mybatis.xml"});
+//        userService = (UserService) context.getBean("userServiceImpl");
+//    }
+//     
+//    @Test
+//    public void addUser(){
+//        User user = new User();
+//        user.setName("你好");
+//        System.out.println(userService.insertUser(user));
+//    }
 	
 	
 }
